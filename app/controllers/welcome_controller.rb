934af355ad25c533f8446d9fa179343d9d9be0d4
lucasleandro1 <lukkassoliveira2215@gmail.com>
2023-10-10 11:48:20 -0300

    add cookie

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails [cookie]"
+    @curso = params[:curso]
     @meu_nome = params[:nome]
   end
 end
